79a2b906a098b8dbaf2555f33f70ce502baadc06
dev <dev@dev-virtual-machine.(none)>
2013-04-25 14:08:38 +0200

    new sftp server

---
+++ core/sftp_fs_server.py
@@ -0,0 +1,49 @@
+import logging
+
+from fs.expose.sftp import * 
+
+import paramiko
+
+class SecureServerInterface(BaseServerInterface):
+    def check_auth_none(self, username):
+        return paramiko.AUTH_FAILED
+    def check_auth_password(self, username, password):
+        return paramiko.AUTH_FAILED
+    def check_auth_publickey(self, username, key):
+        if username == 'test':
+            return paramiko.AUTH_SUCCESSFUL
+        return paramiko.AUTH_FAILED
+    def get_allowed_auths(self, username):
+        return "publickey"
+
+class SecureSFTPRequestHandler(SFTPRequestHandler):
+    def handle(self):
+        self.transport.start_server(server=SecureServerInterface())
+
+class SFTPServer(object):
+    def __init__(self, filesystem, port):
+        self.fs = filesystem
+        self.port = port
+
+    def serve_forever(self):
+        self.server = BaseSFTPServer(('',self.port), self.fs,
+                      RequestHandlerClass=SecureSFTPRequestHandler)
+        try:
+            self.server.serve_forever()
+        except Exception, e:
+            logging.error(e)
+
+    def server_close(self):
+        self.server.server_close()
+
+if __name__ == "__main__":
+    from fs.osfs import OSFS
+    fs = OSFS('.')
+    port = 49152
+    server = SFTPServer(fs, port)
+
+    try:
+        server.serve_forever()
+
+    except SystemExit, KeyboardInterrupt:
+        server.server_close()
